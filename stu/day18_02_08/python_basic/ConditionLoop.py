c8bc8cc942647de605a4a3f9af341290642cb856
suzhuo <1300544374@qq.com>
2018-02-08 23:41:28 +0800

    python循环练习:for while 完毕

--- stu/day18_02_08/python_basic/ConditionLoop.py
+++ stu/day18_02_08/python_basic/ConditionLoop.py
@@ -15,11 +15,35 @@ class ConditionLoop(object):
             print('<=60')
 
     # 循环练习
-    def loopDemo(self):
-        pass
+    def loopDemo(self, maxVal):
+        print('for.............')
+        # range(5):生成5以内的数字不包含5
+        forDemo = list(range(5))
+        for intVal in forDemo:
+            if intVal % 2 == 0:
+                continue
+            print(intVal)
+
+        print('while.............')
+        num = 0
+        while num < 5:
+            if num == 3:
+                break
+            print(num)
+            num += 1
+
+        # 斐波那契数列
+        print('斐波那契数列........')
+        a, b, n = 0, 1, 0
+        while n < maxVal:
+            print(b)
+            a, b = b, a+b
+            n += 1
+
 
 
 if __name__ == '__main__':
     conLoop = ConditionLoop()
 
     conLoop.conditionDemo(100)
+    conLoop.loopDemo(5)
