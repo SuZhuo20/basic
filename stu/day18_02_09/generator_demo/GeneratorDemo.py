9be44d158dc1b1a4f5afa111a73296b9c265030c
gengzhuozhuo <1300544374@qq.com>
2018-02-09 19:31:48 +0800

    生成器，创建list和创建generator方法对比

--- stu/day18_02_09/generator_demo/GeneratorDemo.py
+++ stu/day18_02_09/generator_demo/GeneratorDemo.py
@@ -9,7 +9,20 @@
 #       解决办法：
 #           1.记录列表中数据推算的算法，这样就不需要创建出完整的list，从而节约了内存，python中将这种一遍循环一个推算下个元素的机制成为生成器（generator）
 class GeneratorDemo(object):
-    pass
+
+    # 创建生成器
+    print('create_generator............................................')
+    def create_generator(self):
+        # 创建list
+        listDemo = [x for x in range(10)]
+        print('type [x for x in range(10)]:', type(listDemo))
+        # 创建生成器,在generator中保存的是算法
+        genDemo = (x for x in range(10))
+        print('type (x for x in range(10)):', type(genDemo))
+        # genDemo中元素
+        print('next(generatorObj):', next(genDemo))
 
 if __name__ == '__main__':
     genDemo = GeneratorDemo()
+
+    genDemo.create_generator()
